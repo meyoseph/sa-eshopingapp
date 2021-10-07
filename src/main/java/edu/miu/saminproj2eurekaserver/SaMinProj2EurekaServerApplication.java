31304da68443e598ee9477101fae049169320a88
meyoseph <meyoseph@gmail.com>
2021-10-07 16:36:30 -0500

    initial commit

---
+++ src/main/java/edu/miu/saminproj2eurekaserver/SaMinProj2EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.saminproj2eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaMinProj2EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaMinProj2EurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/miu/saminproj2eurekaserver/SaMinProj2EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.saminproj2eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaMinProj2EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
